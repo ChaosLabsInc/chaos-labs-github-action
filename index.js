3477265897c3023bfa97dd9f6505a96af041a1f6
Omer <arieg419@gmail.com>
2021-09-27 00:49:24 -0400

    Use env secrets and ping chaos ci route

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 const CHAOS_TOKEN = core.getInput('CHAOS_TOKEN');
 const CHAOS_ID = core.getInput('CHAOS_ID');
 const CHAOS_ENDPOINT = core.getInput('CHAOS_ENDPOINT');
 
 async function run() {
     console.log('Hello index.js, world!');
-    const url = `${CHAOS_ENDPOINT}/${CHAOS_ID}&key=${TENOR_TOKEN}`;
+    const url = `${CHAOS_ENDPOINT}/ci/?&CHAOS_ID=${CHAOS_ID}&CHAOS_TOKEN=${CHAOS_TOKEN}`;
     const response = await fetch(url);
     const { results } = await response.json();
     console.log("Got results from API server.... ")
